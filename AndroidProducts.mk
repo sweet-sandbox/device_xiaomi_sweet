5cf05885c349242a6611416cbb760dbe3b4f9365
hdrd13 <96956239+hdrd13@users.noreply.github.com>
2024-01-21 14:06:49 +0200

    sweet: Welcome PixelOS

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/awaken_sweet.mk
+    $(LOCAL_DIR)/aosp_sweet.mk
 
 COMMON_LUNCH_CHOICES := \
-    awaken_sweet-user \
-    awaken_sweet-userdebug \
-    awaken_sweet-eng
+    aosp_sweet-user \
+    aosp_sweet-userdebug \
+    aosp_sweet-eng
